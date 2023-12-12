f73e7322fd92277ee0d63e36db1705122a5fd60b
YenPhamNhu <119447095+YenPhamNhu@users.noreply.github.com>
2023-12-12 20:57:48 +0700

    Fill color RectangleShape

--- SVGDemo/RectangleShape.cpp
+++ SVGDemo/RectangleShape.cpp
@@ -1,8 +1,18 @@
 #include "stdafx.h"
 #include "RectangleShape.h"
 
+//RectangleShape::RectangleShape(int x, int y, int width, int height, int strokeWidth, Gdiplus::Color strokeColor, Gdiplus::Color fillColor, float strokeOpacity, float fillOpacity)
+//    : x(x), y(y), width(width), height(height), strokeWidth(strokeWidth), strokeColor(strokeColor), fillColor(fillColor), strokeOpacity(strokeOpacity), fillOpacity(fillOpacity) {}
+//
+//void RectangleShape::Draw(Gdiplus::Graphics& graphics) {
+//    Gdiplus::Pen pen(strokeColor, strokeWidth * strokeOpacity);
+//    Gdiplus::SolidBrush brush(fillColor);
+//
+//    graphics.DrawRectangle(&pen, x, y, width, height);
+//    graphics.FillRectangle(&brush, x, y, width, height);
+//}
 RectangleShape::RectangleShape(int x, int y, int width, int height, int strokeWidth, Gdiplus::Color strokeColor, Gdiplus::Color fillColor, float strokeOpacity, float fillOpacity)
-    : x(x), y(y), width(width), height(height), strokeWidth(strokeWidth), strokeColor(strokeColor), fillColor(fillColor), strokeOpacity(strokeOpacity), fillOpacity(fillOpacity) {}
+    : FillColor(strokeWidth, strokeColor, fillColor, strokeOpacity, fillOpacity), x(x), y(y), width(width), height(height) {}
 
 void RectangleShape::Draw(Gdiplus::Graphics& graphics) {
     Gdiplus::Pen pen(strokeColor, strokeWidth * strokeOpacity);
--- SVGDemo/RectangleShape.h
+++ SVGDemo/RectangleShape.h
@@ -2,26 +2,38 @@
 #define RECTANGLESHAPE_H
 
 #include "Shape.h"
-
+#include "FillColor.h"
 #include <windows.h>
 #include <gdiplus.h>
 
-class RectangleShape {
+//class RectangleShape {
+//public:
+//    RectangleShape(int x, int y, int width, int height, int strokeWidth, Gdiplus::Color strokeColor, Gdiplus::Color fillColor, float strokeOpacity, float fillOpacity);
+//
+//    void Draw(Gdiplus::Graphics& graphics);
+//
+//private:
+//    int x;
+//    int y;
+//    int width;
+//    int height;
+//    int strokeWidth;
+//    Gdiplus::Color strokeColor;
+//    Gdiplus::Color fillColor;
+//    float strokeOpacity;
+//    float fillOpacity;
+//};
+class RectangleShape : public FillColor, public Shape {
 public:
     RectangleShape(int x, int y, int width, int height, int strokeWidth, Gdiplus::Color strokeColor, Gdiplus::Color fillColor, float strokeOpacity, float fillOpacity);
 
-    void Draw(Gdiplus::Graphics& graphics);
+    void Draw(Gdiplus::Graphics& graphics) override;
 
 private:
     int x;
     int y;
     int width;
     int height;
-    int strokeWidth;
-    Gdiplus::Color strokeColor;
-    Gdiplus::Color fillColor;
-    float strokeOpacity;
-    float fillOpacity;
 };
 
 #endif
